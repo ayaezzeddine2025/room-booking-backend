06e1203861e4e1ed3d2aa3b00e23f7cf2ad9c029
ayaezzeddine <ayaezzd2024@gmail.com>
2026-06-11 16:45:19 +0300

    Update server and database configuration for Railway

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "room_booking_system",
+  host: process.env.MYSQLHOST || "127.0.0.1",
+  port: process.env.MYSQLPORT || 3306,
+  user: process.env.MYSQLUSER || "root",
+  password: process.env.MYSQLPASSWORD || "",
+  database: process.env.MYSQLDATABASE || "room_booking_system",
 });
 
 db.connect((err) => {
